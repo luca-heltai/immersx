14f07d735b28f014c9f9fe1e5f47f7c9a91a2e2c
Luca Heltai <luca.heltai@unipi.it>
2026-08-04 23:06:20 +0200

    Fix resolved thickness field test setup

--- gtests/reduced_field_expressions_01.cc
+++ gtests/reduced_field_expressions_01.cc
@@ -98,6 +98,7 @@ TEST(TensorProductSpace, ThicknessExpressionUsesAResolvedBinding)
   TensorProductSpaceParameters<1, 2, 3, 1> parameters;
   parameters.reduced_grid_name =
     SOURCE_DIR "/data/tests/one_cylinder_properties.vtk";
+  parameters.input_file_fields = "radius";
   parameters.thickness = "radius";
 
   TensorProductSpace<1, 2, 3, 1> space(parameters);
