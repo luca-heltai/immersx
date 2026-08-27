c440c4b53260ea0ca670032f0172aa458090a46d
Luca Heltai <luca.heltai@unipi.it>
2026-08-27 19:14:40 +0200

    refactor: install one sampling plan per FE source

--- include/immersx/io/imported_finite_element_fields.h
+++ include/immersx/io/imported_finite_element_fields.h
@@ -94,12 +94,6 @@ namespace ImmersX
         return storage_->coefficients;
       }
 
-      std::shared_ptr<const VectorType>
-      coefficients_handle() const
-      {
-        return coefficients_;
-      }
-
       const ReducedFieldDescriptor &
       descriptor() const
       {
