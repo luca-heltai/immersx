2dd740b348970ce667fa11ba70640ec1c500b62c
Luca Heltai <luca.heltai@unipi.it>
2026-08-29 23:06:02 +0200

    Expose common Schur operations through IDA

--- include/immersx/core/sundials_ida_adapter.h
+++ include/immersx/core/sundials_ida_adapter.h
@@ -12,6 +12,9 @@
 
 #include <deal.II/base/config.h>
 
+#include <deal.II/lac/precondition.h>
+#include <deal.II/lac/solver_gmres.h>
+
 #include <immersx/core/detail/execution_composition.h>
 #include <immersx/core/problem_handle.h>
 #include <immersx/core/representation.h>
@@ -34,10 +37,13 @@ namespace ImmersX
   class IDAAdapter
   {
   public:
+    using Composition =
+      detail::ExecutionComposition<FieldVectorType, GlobalVectorType>;
     using RepresentationType = Representation<FieldVectorType>;
     using ComponentRepresentationType =
       ComponentRepresentation<FieldVectorType>;
     using Operator            = dealii::LinearOperator<GlobalVectorType>;
+    using LocalOperator       = dealii::LinearOperator<FieldVectorType>;
     using LinearSolveFunction = std::function<void(const Operator &,
                                                    const GlobalVectorType &,
                                                    GlobalVectorType &,
@@ -47,15 +53,11 @@ namespace ImmersX
 
     IDAAdapter(const AdditionalData &data,
                const MPI_Comm        communicator,
-               LinearSolveFunction   solve)
+               LinearSolveFunction   solve = {})
       : composition_(communicator)
       , solve_(std::move(solve))
       , ida_(data, communicator)
-    {
-      AssertThrow(solve_,
-                  dealii::ExcMessage("IDAAdapter requires a linear solve "
-                                     "callback."));
-    }
+    {}
 
     template <typename Problem, typename... Arguments>
     auto
@@ -168,10 +170,69 @@ namespace ImmersX
       return *current_jacobian_;
     }
 
-  private:
-    using Composition =
-      detail::ExecutionComposition<FieldVectorType, GlobalVectorType>;
+    bool
+    can_materialize_matrix(const GlobalVectorType &state,
+                           const GlobalVectorType &state_dot,
+                           const double            alpha) const
+    {
+      return composition_.can_materialize_matrix(state, &state_dot, alpha);
+    }
 
+    typename Composition::BlockMatrixType
+    block_matrix(const GlobalVectorType &state,
+                 const GlobalVectorType &state_dot,
+                 const double            alpha) const
+    {
+      return composition_.block_matrix(state, &state_dot, alpha);
+    }
+
+    typename Composition::MatrixType
+    monolithic_matrix(const GlobalVectorType &state,
+                      const GlobalVectorType &state_dot,
+                      const double            alpha) const
+    {
+      return composition_.monolithic_matrix(
+        composition_.block_matrix(state, &state_dot, alpha));
+    }
+
+    bool
+    has_local_preconditioner(const FieldId field) const
+    {
+      return composition_.has_local_preconditioner(field);
+    }
+
+    LocalOperator
+    local_preconditioner(const FieldId           field,
+                         const GlobalVectorType &state) const
+    {
+      const auto result = composition_.local_preconditioner(field, state);
+      AssertThrow(result.has_value(),
+                  dealii::ExcMessage("No local preconditioner is registered "
+                                     "for this Field."));
+      return *result;
+    }
+
+    const std::vector<SaddlePointMetadata> &
+    saddle_points() const
+    {
+      return composition_.saddle_points();
+    }
+
+    LocalOperator
+    schur_operator(const FieldId           multiplier,
+                   const GlobalVectorType &state) const
+    {
+      return composition_.schur_operator(multiplier, state);
+    }
+
+    Operator
+    schur_preconditioner(const FieldId           multiplier,
+                         const GlobalVectorType &state) const
+    {
+      return composition_.schur_preconditioner(multiplier, state);
+    }
+
+  private:
     void
     finalize()
     {
@@ -201,7 +262,17 @@ namespace ImmersX
         AssertThrow(current_jacobian_.has_value(),
                     dealii::ExcMessage("IDA requested a solve without a "
                                        "current Jacobian."));
-        solve_(*current_jacobian_, rhs, dst, tolerance);
+        if (solve_)
+          solve_(*current_jacobian_, rhs, dst, tolerance);
+        else
+          {
+            dealii::SolverControl                 control(1000, tolerance);
+            dealii::SolverGMRES<GlobalVectorType> solver(control);
+            solver.solve(*current_jacobian_,
+                         dst,
+                         rhs,
+                         dealii::PreconditionIdentity());
+          }
       };
       ida_.differential_components = [this]() {
         return composition_.differential_components();
